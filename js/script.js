14874853084a301e4da0dfab1ffa5dd845a8e37f
shaik1729 <shaiktajuddinshavali@gmail.com>
2021-05-27 13:25:35 +0530

    reload fix

--- js/script.js
+++ js/script.js
@@ -1,7 +1,5 @@
-// console.log("Hello World!");
-
 var noc = 10;
-const reqnum = Math.floor(Math.random() * 100);
+const reqnum = Math.floor(Math.random() * 100); 
 
 var getnum = document.getElementById('Number');
 var showresult = document.getElementById('result');
@@ -13,14 +11,11 @@ var marchanges = document.getElementById('left_chance');
 
 function checkResult(){
     let num = getnum.value;
-    // console.log("user input : ", num);
-    // console.log("random num : ", reqnum);
     if(num == reqnum){
         imgChange1.src = "./images/congrats.gif";
         imgChange2.src = "./images/congrats.gif";
         showresult.innerText = "You Got it!!";
-        alert("Congrates You got it Play again!!");
-        location.reload();
+        document.getElementById('replay').innerHTML = '<a href="" onclick="replay()">Play Again!!</a>';
     } else if((num < reqnum-10) || (num > reqnum+10)){
         imgChange1.src = "./images/far.gif";
         imgChange2.src = "./images/far.gif";
@@ -40,8 +35,6 @@ function checkResult(){
 }
 
 function reduceCount(){
-    // console.log("current count : ",noc);
-
      if(noc == 1){
          countofchances.innerText = String(noc);
          showresult.innerText = "You Lost Try again!!";
@@ -52,5 +45,8 @@ function reduceCount(){
         countofchances.innerText = String(noc);
         marchanges.innerText = "Guess The Number Between 1 to 100 You Have "+ String(noc) + " Chances left !!";
      }
-    //  console.log("after count : ",noc);
+}
+
+function replay(){
+    location.reload();
 }
